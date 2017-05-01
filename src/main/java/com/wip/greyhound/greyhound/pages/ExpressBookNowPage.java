9fa21240b90005aecfa03fe0311ee8849537ee76
aan <aanpudur@gmail.com>
2017-05-01 08:25:09 -0700

    book a trip

--- src/main/java/com/wip/greyhound/greyhound/pages/ExpressBookNowPage.java
+++ src/main/java/com/wip/greyhound/greyhound/pages/ExpressBookNowPage.java
@@ -2,23 +2,31 @@ package com.wip.greyhound.greyhound.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExpressBookNowPage extends BasePage {
-  String region, fromLocation, toLocation, DepartDate, passenger;
+  String region, fromLocationName, toLocationName, DepartDate, passenger;
+
+  // wait
+  WebDriverWait wait = new WebDriverWait(driver, 8);
+
+  // xpaths
+  private String regionXpath = "//div/ul/li[1][text()=";
+  private String fromLocationXpath ="//*[@id='ctl00_body_listOrigin_DropDown']/div/ul/li[text()=";
+
 
   public ExpressBookNowPage(WebDriver driver) {
     super(driver);
   }
 
-
-  public ExpressBookNowPage(String region, String fromLocation, String toLocation,
+  public ExpressBookNowPage(String region, String fromLocationName, String toLocationName,
       String DepartDate, String passenger) {
     super(driver);
     this.region = region;
-    this.fromLocation = fromLocation;
-    this.toLocation = toLocation;
+    this.fromLocationName = fromLocationName;
+    this.toLocationName = toLocationName;
     this.DepartDate = DepartDate;
     this.passenger = passenger;
   }
@@ -28,27 +36,32 @@ public class ExpressBookNowPage extends BasePage {
     if (roundTripBtnEnabled) {
       driver.findElement(By.id("rbROUND_TRIP")).click();
     }
-
-
   }
 
   public void setRegion(String region) {
     this.region = region;
     driver.findElement(By.id("ctl00_body_listRegion_Arrow")).click();
-    WebDriverWait wait = new WebDriverWait(driver, 3);
+
     wait.until(
-        ExpectedConditions.elementToBeClickable(By.xpath("//div/ul/li[1][text()=" + region + "]")))
+        ExpectedConditions.elementToBeClickable(By.xpath(regionXpath + "\'" + region + "\'" + "]")))
         .click();
   }
 
 
-  public void setFromLocation(String fromLocation) {
-    this.fromLocation = fromLocation;
+  public void setFromLocation(String fromLocation) throws InterruptedException {
+    this.fromLocationName = fromLocation;  
+//    wait.until(ExpectedConditions.elementToBeSelected(fromLocationXpath));
+//    driver.findElement(By.xpath("//a[@id='ctl00_body_listOrigin_Arrow']")).click();
+//   
+
+    wait.until(ExpectedConditions
+        .elementToBeClickable(By.xpath(fromLocationXpath + "\'" + fromLocationName + "\'" + "]")))
+        .click();
   }
 
 
   public void setToLocation(String toLocation) {
-    this.toLocation = toLocation;
+    this.toLocationName = toLocation;
   }
 
 
@@ -63,15 +76,6 @@ public class ExpressBookNowPage extends BasePage {
 
 
 
-  // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-  // Thread.sleep(4000);
-  //
-  // driver.findElement(By.xpath("//*[@id='ctl00_body_listOrigin_Arrow']")).click();
-  //
-  // driver
-  // .findElement(
-  // By.xpath("//*[@id='ctl00_body_listOrigin_DropDown']/div/ul/li[text()='Red Deer']"))
-  // .click();
   //
   // driver.findElement(By.id("ctl00_body_listDestination_Arrow")).click();
 
